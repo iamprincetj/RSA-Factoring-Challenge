a6676df5100ab480301d430ec99ba74858ca5018
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-02 15:38:35 +0000

    updated

--- factorise.py
+++ factorise.py
@@ -2,6 +2,9 @@
 
 def factorize(num_str):
     for i in range(2, int(num_str ** 0.5) + 1):
-        if num_str % i == 0:
-            print(f"{num_str}={num_str//i}*{i}")
-            return
+        if num_str % 2 == 0:
+	    break
+        else:
+            if num_str % i == 0:
+    print(f"{num_str}={num_str//i}*{i}")
+    return
